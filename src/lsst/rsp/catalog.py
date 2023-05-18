dc6437fc8ebe47fe030cf14542438c1804e62cec
Christine Banek <cbanek@lsst.org>
2023-05-18 14:33:42 -0700

    [DM-39277] Add wrappers for sso tap

--- src/lsst/rsp/catalog.py
+++ src/lsst/rsp/catalog.py
@@ -28,6 +28,15 @@ def _get_obstap_url() -> str:
     return obstapurl
 
 
+def _get_ssotap_url() -> str:
+    ssotapurl = os.getenv("EXTERNAL_SSOTAP_URL")
+    if not ssotapurl:
+        ssotapurl = (os.getenv("EXTERNAL_INSTANCE_URL") or "") + (
+            os.getenv("SSOTAP_ROUTE") or "/api/ssotap"
+        )
+    return ssotapurl
+
+
 def _get_datalink_url() -> str:
     return os.getenv("EXTERNAL_INSTANCE_URL", "") + "/api/datalink"
 
@@ -39,6 +48,7 @@ def _get_cutout_url() -> str:
 def _get_auth() -> Optional[pyvo.auth.authsession.AuthSession]:
     tap_url = _get_tap_url()
     obstap_url = _get_obstap_url()
+    ssotap_url = _get_ssotap_url()
     s = requests.Session()
     tok = get_access_token()
     if not tok:
@@ -56,6 +66,10 @@ def _get_auth() -> Optional[pyvo.auth.authsession.AuthSession]:
     auth.add_security_method_for_url(obstap_url + "/sync", "lsst-token")
     auth.add_security_method_for_url(obstap_url + "/async", "lsst-token")
     auth.add_security_method_for_url(obstap_url + "/tables", "lsst-token")
+    auth.add_security_method_for_url(ssotap_url, "lsst-token")
+    auth.add_security_method_for_url(ssotap_url + "/sync", "lsst-token")
+    auth.add_security_method_for_url(ssotap_url + "/async", "lsst-token")
+    auth.add_security_method_for_url(ssotap_url + "/tables", "lsst-token")
     return auth
 
 
@@ -86,6 +100,21 @@ def get_obstap_service() -> pyvo.dal.TAPService:
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=UserWarning)
         ts = pyvo.dal.TAPService(_get_obstap_url(), _get_auth())
+    return ts
+
+
+def get_ssotap_service() -> pyvo.dal.TAPService:
+    #
+    # This is not ideal, but warning appears because require pyvo does
+    # not register uws:Sync and uws:Async.  It's harmless.  The broadness
+    # of the warning is unfortunately necessary since pyvo just uses
+    # warnings.warn():
+    # https://github.com/astropy/pyvo/blob/
+    # 81a50d7fd24428f17104a075bc0e1ac661ed6ea0/pyvo/utils/xml/elements.py#L418
+    #
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=UserWarning)
+        ts = pyvo.dal.TAPService(_get_ssotap_url(), _get_auth())
     return ts
 
 
