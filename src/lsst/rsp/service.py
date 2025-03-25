5081477f28125d017f2335162015331ace49515e
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2025-03-24 20:31:27 -0700

    Modify get_siav2_service to work with new SIA endpoint

--- src/lsst/rsp/service.py
+++ src/lsst/rsp/service.py
@@ -1,5 +1,7 @@
 """Utility functions for IVOA clients."""
 
+import os
+
 from pyvo.dal import SIA2Service
 from pyvo.dal.adhoc import DatalinkResults
 from pyvo.dal.sia2 import ObsCoreRecord
@@ -14,11 +16,29 @@ def get_datalink_result(result: ObsCoreRecord) -> DatalinkResults:
     )
 
 
-def get_siav2_service(label: str) -> SIA2Service:
+def get_siav2_service(data_release: str) -> SIA2Service:
     """Construct an `SIA2Service` client."""
-    if label != "staff":
-        raise ValueError(label + " data not available at your location")
+    # data_release determines the Data release, as we may have different
+    # releases being served from the same server.
 
-    # No matter what, we've only got one sia server per environment
-    # so for now just do some checking.
-    return SIA2Service(get_service_url("siav2"), get_pyvo_auth())
+    label = os.getenv("RSP_SITE_TYPE", None)
+
+    # The label variable here corresponds to RSP "kind" (i.e. "telescope",
+    # "science", "staff")
+
+    if label not in {"science", "staff"}:
+        if label is None:
+            raise ValueError("RSP_SITE_TYPE environment variable is not set")
+        raise ValueError(f"{label} data not available at your location")
+
+    sia_url = get_service_url(f"sia/{data_release}")
+    if not sia_url:
+        raise RuntimeError(
+            f"Failed to determine service URL for data release: {data_release}"
+        )
+
+    session = get_pyvo_auth()
+    if session:
+        session.add_security_method_for_url(sia_url + "/query", "lsst-token")
+
+    return SIA2Service(sia_url, session=session)
