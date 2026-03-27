11e770c271d32cbbb70890a3ffeb7a0ca3b13832
Russ Allbery <rra@lsst.org>
2026-03-26 18:23:57 -0700

    Remove discovery_v1_path argument to RSPDiscovery
    Use pyfakefs for discovery testing so that the discovery information
    can be mounted at the appropriate path, and then drop the test-only
    `discovery_v1_path` argument to `RSPDiscovery` so that it doesn't have
    to be documented.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,29 @@
 from pathlib import Path
 
 import pytest
+from pyfakefs.fake_filesystem import FakeFilesystem
 from safir.testing.data import Data
+
+from lsst.rsp import RSPDiscovery
 
 
 @pytest.fixture
-def data() -> Data:
-    return Data(Path(__file__).parent / "data")
+def data(fs: FakeFilesystem) -> Data:
+    fs.add_real_directory(Path(__file__).parent / "data")
+    return Data(Path(__file__).parent / "data", fake_filesystem=fs)
+
+
+@pytest.fixture
+def discovery_path(data: Data, fs: FakeFilesystem) -> Path:
+    """Set up a fake file system with discovery in the expected path."""
+    path = data.path("discovery/v1.json")
+    fs.add_real_file(path, target_path=RSPDiscovery._DISCOVERY_PATH)
+    return path
 
 
 @pytest.fixture
 def discovery_v1_path() -> Path:
+    """Delete once older functions and tests are retired."""
     return Path(__file__).parent / "data" / "discovery" / "v1.json"
 
 
